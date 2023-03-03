da3d8108b2088a3bb71bbcae7d131d3cdab83c71
Artem Riabchych <artemskyart@gmail.com>
2023-03-03 14:47:36 +0200

    add-Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Goit-react-hw-03-image-finder:)
-    </div>
-  );
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    search: 'start',
+    page: 1,
+  };
+
+  handleInput = ( state ) => {
+    console.log(state);
+    this.setState(state)
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar handleInput={this.handleInput} />
+        <h1>{this.state.search}</h1>
+      </div>
+    );
+  }
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,51 @@
+import { Component } from "react";
+import { BiSearch } from 'react-icons/bi';
+import css from './Searchbar.module.css'
+
+export class Searchbar extends Component {
+  state = {
+    search: '',
+  };
+
+  // <BiSearch/> or >> 🔍
+
+    onChangeInput = (evt) => {
+        const { name, value } = evt.currentTarget;
+        this.setState({ [name]: value });
+  }
+
+    resetForm = () => {
+     this.setState({ search: '' });
+    }
+  render() {
+    return (
+      <header className={css.searchbar}>
+            <form onSubmit={(evt) => {
+                evt.preventDefault();
+                this.props.handleInput(this.state)
+                 this.resetForm();
+            }
+                
+            } className={css.form}>
+                <button type="submit"
+                    className={css.button}>
+            <span className={css.buttonLabel}>
+              <BiSearch size="20" />
+            </span>
+          </button>
+
+          <input
+            value={this.state.search}
+            onChange={this.onChangeInput}
+            className={css.input}
+            name="search"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
